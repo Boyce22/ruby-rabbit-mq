eb6afaffb667a6c7f8a24ff42463a3caa0c3180b
Bernardo Alves <bernardojob2003@outlook.com>
2024-11-20 21:33:24 -0300

    fix(user): construção do usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@
 class User < ApplicationRecord
     attr_accessor :name
 
-    validates: :first_name, :email, :presence: true,
-    validates: :email, uniqueness: true
+    validates :first_name, :email, presence: true
+    validates :email, uniqueness: true
 
-    def name(nome)
-        nome = nome.split(' ')
-       self[:first_name] = nome.shift
-       self[:last_name] = nome.join(' ')
+    def name=(name)
+        name = name.split(' ')
+        self[:first_name] = name.shift
+        self[:last_name] = name.join(' ')
     end
 
     def name
